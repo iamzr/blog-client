601ad79c71e258cf4e33913efc7b0955042c2aa5
Zia Rehman <iamziarehman@gmail.com>
2021-10-16 18:37:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import("./styles.css");
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="title">My Simple Blog</div>
+      </header>
+      <div className="summary">
+        Some stuff a summary of your blog and all that
+      </div>
+      <div className="post-list"></div>
+    </div>
+  );
+}
+
+export default App;
